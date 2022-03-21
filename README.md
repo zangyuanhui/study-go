7ac49dd8dd0364f3541c059df2e596a1ad353203
zangyuanhui <zangyuanhui@jwzg.com>
2022-03-21 14:51:42 +0800

    范型go>1.18

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # study-go
 关于一些go语言学习
+## 常用命令
+- go version
+- go help
+- go mod init
+- go mod tidy
+- go clean -modcache
+- go run .
+- go build
+- go install
+- go test -v
+- go get .
---
+++ generics/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+func SumInts(m map[string]int64) int64 {
+	var s int64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SumFloats(m map[string]float64) float64 {
+	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+//func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+//	var s V
+//	for _, v := range m {
+//		s += v
+//	}
+//	return s
+//}
+
+//type Number interface {
+//	int64 | float64
+//}
+
+//func SumNumbers[K comparable, V Number](m map[K]V) V {
+//	var s V
+//	for _, v := range m {
+//		s += v
+//	}
+//	return s
+//}
+
+func main() {
+	ints := map[string]int64{
+		"first":  1,
+		"second": 2,
+	}
+
+	floats := map[string]float64{
+		"first":  3.3,
+		"second": 4.4,
+	}
+
+	fmt.Printf("Non-Generic Sums:%v and %v", SumInts(ints), SumFloats(floats))
+	//fmt.Printf("Generic Sums: %v and %v\n", SumIntsOrFloats[string, int64](ints), SumIntsOrFloats[string, float64](floats))
+	//fmt.Printf("Generic Sums with Constraint: %v and %v\n", SumNumbers(ints), SumNumbers(floats))
+}
